da742675e2063b9f6e3fb1709d066a4f6d52f5e5
izakhan <121670888+izakhan@users.noreply.github.com>
2023-02-18 14:26:23 +0530

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -101,7 +101,7 @@ bot
     await ctx.reply(ctx.t("start-msg", { user: ctx.from.first_name }), {
       parse_mode: "HTML",
       reply_markup: new InlineKeyboard()
-        .url(ctx.t("𝗡𝗲𝘄 𝗖𝗶𝗻𝗲𝗺𝗮𝗦 ~ 𝗨𝗻𝗹𝗶𝗺𝗶𝘁𝗲𝗱🎭™"), "https://t.me/+nQjZWOoEVpMxOWE1").row
+        .url(ctx.t("𝗡𝗲𝘄 𝗖𝗶𝗻𝗲𝗺𝗮𝗦 ~ 𝗨𝗻𝗹𝗶𝗺𝗶𝘁𝗲𝗱🎭™"), "https://t.me/+nQjZWOoEVpMxOWE1")
         .url(ctx.t("ᴄɪɴᴇᴍᴀ ʀᴏᴄᴋᴇʀs🎭™"), "https://t.me/+cm1FYByZ5SgzYjc1"),
       disable_web_page_preview: true,
     });
